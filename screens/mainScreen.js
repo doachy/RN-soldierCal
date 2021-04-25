a81af6481f91d3562333bbd41a57032f1c0dc675
doachy <dnwjd2839@naver.com>
2021-04-25 14:22:49 +0900

    add the caculate.js in screen

--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -1,10 +1,36 @@
 import * as React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import Calculate from './calculate';
 
 export default function MainScreen({ navigation }) {
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Button onPress={() => navigation.navigate('share')} title="go to share" />
+		<View style={styles.container}>
+			<View style={styles.topContainer}>
+				<Text>상병 김륜기</Text>
+				<Calculate/>
+			</View>
+			<View style={styles.bottomContainer}>
+				<Text>전역일 D-160</Text>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	topContainer: {
+		flex: 2,
+		alignItems: 'center',
+		justifyContent: 'center',
+		color: 'blue',
+	},
+	bottomContainer: {
+		flex: 3,
+		alignItems: 'center',
+		color: 'red',
+	},
+});
