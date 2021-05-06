95211fc91a740aabf5dbfea85433e3662513f049
doachy <dnwjd2839@naver.com>
2021-05-06 19:20:34 +0900

    add the push button for change the screen

--- screens/calculateClock.js
+++ screens/calculateClock.js
@@ -9,7 +9,7 @@ class CalculateClock extends React.Component {
 		clockTimeMins: '0',
 		clockTimeSecs: '0',
 		clockTime1: '0',
-		num: '0',
+		num: 0,
 	};
 
 	intervalId;
@@ -18,6 +18,7 @@ class CalculateClock extends React.Component {
 	componentDidMount() {
 		const finalDay = new Date('2021-10-01');
 		const startDay = new Date('2020-06-01');
+		
 
 		this.intervalId = setInterval(() => {
 			this.countDayFN(finalDay, startDay);
@@ -61,23 +62,29 @@ class CalculateClock extends React.Component {
 
 	 handleClick() {
     this.setState(() => {
-      return {num: this.state.num + 1};
+		if(this.state.num>4){
+			this.setState({
+				num: 0,
+			})
+		} else {
+			return {num: this.state.num + 1};
+		}
     });
   };
 
 	render() {
 		const { clockTimeHours, clockTimeMins, clockTimeSecs, clockTime1, num } = this.state;
-		const conditions = ['EarlyMorning', 'Morning', 'Sunrise', 'Afternoon', 'Sunset', 'Midnight'];
-
+		const conditions = ['EarlyMorning', 'Sunrise', 'Morning', 'Afternoon', 'Sunset', 'Midnight'];
 		return (
 			<View style={styles.container}>
-				<Scenery condition={conditions[4]} />
+				<Scenery condition={conditions[this.state.num]} />
 				<Clock hours={clockTimeHours} mins={clockTimeMins} secs={clockTimeSecs} />
 				<View style={styles.textbox}>
 					<Button title="push" onPress={() => this.handleClick()}></Button>
 					<Text style={{ marginTop: 20, color: 'white' }}>clockTime={clockTime1}</Text>
 					<Text style={styles.text}>clockTimeHours={clockTimeHours}</Text>
 					<Text style={styles.text}>clockTimeMins={clockTimeMins}</Text>
+					<Text style={styles.text}>num={num}</Text>
 					<Text style={styles.text}>clockTimeSecs={clockTimeSecs}</Text>
 					<Text style={{ marginTop: 20, color: 'white' }}>
 						*국방 시계는 실제 시간보다 훨씬 느리게 흘러갑니다*
--- screens/scenery.js
+++ screens/scenery.js
@@ -3,64 +3,67 @@ import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
 import { LinearGradient } from 'expo-linear-gradient';
 
-const sceneryOption = {
-	EarlyMorning: {
-		gradient: ['#000428', '#004e92', '#004e92'], //done frost ㅇㅇ
-		title: 'early Morning',
-		subtitle: 'have a good day',
-		statusBar: 'dark-content',
-	},
-	Morning: {
-		gradient: ['#2980B9', '#6DD5FA', '#FFFFFF'], //done cool sky ㅇㅇ
-		title: 'Morning',
-		subtitle: 'have a good day',
-		statusBar: 'dark-content',
-	},
-	Sunrise: {
-		gradient: ['#003973', '#6DD5FA', '#F2A1A0'], //done ㅇㅇ
-		title: 'Sunrise',
-		subtitle: 'its time to get up!',
-		statusBar: 'dark-content',
-	},
-	Afternoon: {
-		gradient: ['#0082c8', '#0082c8', '#FFFFFF'], // done hydrogen ㅇㅇ
-		title: 'Afternoon',
-		subtitle: 'time to go out',
-		statusBar: 'dark-content',
-	},
-	Sunset: {
-		gradient: ['#0B486B', '#0B486B', '#F56217'], //done sunset ㅇㅇ
-		title: 'Sunset',
-		subtitle: 'time to have dinner',
-		statusBar: 'dark-content',
-	},
-	Midnight: {
-		gradient: ['#000428', '#000428', '#004E92'], //done frost ㅇㅇ
-		title: 'Midnight',
-		subtitle: 'its time to go sleep',
-		statusBar: 'light-content',
-	},
-};
+export default class Scenery extends React.Component {
+	render() {
+		let { condition } = this.props;
 
-export default function Scenery({ condition }) {
-	return (
-		<View style={styles.container}>
-			<LinearGradient
-				// Background Linear Gradient
-				colors={[
-					sceneryOption[condition].gradient[0],
-					sceneryOption[condition].gradient[1],
-					sceneryOption[condition].gradient[2],
-				]}
-				style={styles.background}
-			/>
-			<StatusBar barStyle={sceneryOption[condition].statusBar} />
-			<View style={styles.textbox}>
-				<Text style={styles.title}>{sceneryOption[condition].title}</Text>
-				<Text style={styles.subtitle}>{sceneryOption[condition].subtitle}</Text>
+		const sceneryOption = {
+			EarlyMorning: {
+				gradient: ['#000428', '#004e92', '#004e92'], //done frost ㅇㅇ
+				title: 'dawn',
+				subtitle: 'get up',
+				statusBar: 'dark-content',
+			},
+			Morning: {
+				gradient: ['#2980B9', '#6DD5FA', '#FFFFFF'], //done cool sky ㅇㅇ
+				title: 'Morning',
+				subtitle: 'have a good day',
+				statusBar: 'dark-content',
+			},
+			Sunrise: {
+				gradient: ['#003973', '#6DD5FA', '#F2A1A0'], //done ㅇㅇ
+				title: 'Sunrise',
+				subtitle: 'its time to get up!',
+				statusBar: 'dark-content',
+			},
+			Afternoon: {
+				gradient: ['#0082c8', '#0082c8', '#FFFFFF'], // done hydrogen ㅇㅇ
+				title: 'Afternoon',
+				subtitle: 'time to go out',
+				statusBar: 'dark-content',
+			},
+			Sunset: {
+				gradient: ['#0B486B', '#0B486B', '#F56217'], //done sunset ㅇㅇ
+				title: 'Sunset',
+				subtitle: 'time to have dinner',
+				statusBar: 'dark-content',
+			},
+			Midnight: {
+				gradient: ['#000428', '#000428', '#004E92'], //done frost ㅇㅇ
+				title: 'Midnight',
+				subtitle: 'its time to go sleep',
+				statusBar: 'light-content',
+			},
+		};
+		return (
+			<View style={styles.container}>
+				<LinearGradient
+					// Background Linear Gradient
+					colors={[
+						sceneryOption[condition].gradient[0],
+						sceneryOption[condition].gradient[1],
+						sceneryOption[condition].gradient[2],
+					]}
+					style={styles.background}
+				/>
+				<StatusBar barStyle={sceneryOption[condition].statusBar} />
+				<View style={styles.textbox}>
+					<Text style={styles.title}>{sceneryOption[condition].title}</Text>
+					<Text style={styles.subtitle}>{sceneryOption[condition].subtitle}</Text>
+				</View>
 			</View>
-		</View>
-	);
+		);
+	}
 }
 
 Scenery.propTypes = {
@@ -73,15 +76,18 @@ Scenery.propTypes = {
 		'Midnight',
 	]).isRequired,
 };
+Scenery.defaultProps = {
+	condition: 'Morning',
+};
 
 const styles = StyleSheet.create({
 	container: {
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-	textbox:{
-		marginTop:200,
-		marginBottom:30,
+	textbox: {
+		marginTop: 200,
+		marginBottom: 30,
 	},
 	hour: {
 		fontSize: 36,
