58c2dc5ee91c0689ae4314a2b6d2053158b443de
doachy <dnwjd2839@naver.com>
2021-05-16 17:32:37 +0900

    add the input ui

--- screens/clockScreen.js
+++ screens/clockScreen.js
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, StatusBar, SafeAreaView, ScrollView, Button } from 'react-native';
+import {
+	View,
+	Text,
+	StyleSheet,
+	StatusBar,
+	SafeAreaView,
+	KeyboardAvoidingView,
+	ScrollView,
+	TouchableOpacity,
+	Button,
+	Keyboard,
+	TextInput,
+} from 'react-native';
 import ClockContainer from '../components/clockContainer';
 import ProgressBar from '../components/progressBar';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -13,7 +25,9 @@ export default function ClockScreen({ navigation }) {
 	const conditions = ['EarlyMorning', 'Sunrise', 'Morning', 'Afternoon', 'Sunset', 'Midnight'];
 	const [num, setNum] = useState(0);
 	const [start, setStart] = useState('2020-06-01');
+	const [prevstart, setPrevstart] = useState('2020-06-01');
 	const [final, setFinal] = useState('2021-09-24');
+	const [prevfinal, setPrevfinal] = useState('2021-09-24');
 	const [taskItems, setTaskItems] = useState({
 		timeNow: '', //0
 		timeGoal: '', //1
@@ -31,17 +45,24 @@ export default function ClockScreen({ navigation }) {
 		totalDay: '', //13
 		perDay: '',
 	});
-
+	let finalDay = new Date(final);
+	let startDay = new Date(start);
 	const condition = conditions[num];
 
 	useEffect(() => {
-		const finalDay = new Date(final);
-		const startDay = new Date(start);
-
 		intervalId = setInterval(() => {
 			countDayFN(finalDay, startDay);
-		}, 200);
+		}, 100);
 	}, []);
+
+	useEffect(() => {
+		if (start !== '2020-06-01') {
+			clearInterval(intervalId);
+			intervalId = setInterval(() => {
+				countDayFN(finalDay, startDay);
+			}, 4000);
+		}
+	}, [start]);
 
 	const sceneryOption = {
 		EarlyMorning: {
@@ -149,14 +170,22 @@ export default function ClockScreen({ navigation }) {
 			} //14
 		);
 	};
-	
+
 	const handleClick = () => {
-		if(num>4){
-			setNum(0)
+		if (num > 4) {
+			setNum(0);
 		} else {
-			setNum(num + 1)
+			setNum(num + 1);
 		}
-	}
+	};
+
+	const handleAddTask = () => {
+		Keyboard.dismiss();
+		setStart(prevstart);
+		setFinal(prevfinal);
+		finalDay = new Date(final);
+		startDay = new Date(start);
+	};
 
 	return (
 		<View style={styles.container}>
@@ -193,6 +222,8 @@ export default function ClockScreen({ navigation }) {
 						<Text style={styles.text}>clockTimeMins={taskItems['clockTimeMins']}</Text>
 						<Text style={styles.text}>clockTimeSecs={taskItems['clockTimeSecs']}</Text>
 						<Text style={styles.text}>num={num}</Text>
+						<Text style={styles.text}>start={start}</Text>
+						<Text style={styles.text}>final={final}</Text>
 						<Text style={{ marginTop: 20, color: 'white' }}>
 							*국방 시계는 실제 시간보다 훨씬 느리게 흘러갑니다*
 						</Text>
@@ -211,6 +242,31 @@ export default function ClockScreen({ navigation }) {
 						<ProgressBar per={taskItems['perDay']} />
 					</View>
 				</ScrollView>
+
+				{/* Write a task */}
+				{/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+				<KeyboardAvoidingView
+					behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+					style={styles.writeTaskWrapper}
+				>
+					<TextInput
+						style={styles.input}
+						placeholder={'Write a task'}
+						value={prevstart}
+						onChangeText={(text) => setPrevstart(text)}
+					/>
+					<TextInput
+						style={styles.input}
+						placeholder={'Write a task'}
+						value={prevfinal}
+						onChangeText={(text) => setPrevfinal(text)}
+					/>
+					<TouchableOpacity onPress={() => handleAddTask()}>
+						<View style={styles.addWrapper}>
+							<Text style={styles.addText}>+</Text>
+						</View>
+					</TouchableOpacity>
+				</KeyboardAvoidingView>
 			</SafeAreaView>
 		</View>
 	);
@@ -258,4 +314,32 @@ const styles = StyleSheet.create({
 		fontSize: 15,
 		color: '#fff',
 	},
+	writeTaskWrapper: {
+		position: 'absolute',
+		bottom: 60,
+		width: '100%',
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+	},
+	input: {
+		paddingVertical: 15,
+		paddingHorizontal: 15,
+		backgroundColor: '#FFF',
+		borderRadius: 60,
+		borderColor: '#C0C0C0',
+		borderWidth: 1,
+		width: 250,
+	},
+	addWrapper: {
+		width: 60,
+		height: 60,
+		backgroundColor: '#FFF',
+		borderRadius: 60,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderColor: '#C0C0C0',
+		borderWidth: 1,
+	},
+	addText: {},
 });
