a9a50da6ab8def88425bfac2356e6e37cd5c71e3
doachy <dnwjd2839@naver.com>
2021-05-20 22:49:08 +0900

    nested stack of clock and notice screens

--- stack.js
+++ stack.js
@@ -1,21 +1,46 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import ClockScreen from './screens/clockScreen';
 import NoticeScreen from './screens/noticeScreen';
 
-
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={{
-						headerShown: false,}
-					}>
-      <Stack.Screen name="clock" component={ClockScreen} />
-      <Stack.Screen name="notice" component={NoticeScreen} />
-    </Stack.Navigator>
-  );
-}
+	const config = {
+		animation: 'spring',
+		config: {
+			stiffness: 1000,
+			damping: 500,
+			mass: 3,
+			overshootClamping: true,
+			restDisplacementThreshold: 0.01,
+			restSpeedThreshold: 0.01,
+		},
+	};
+	
+	const forFade = ({ current }) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
+	
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="clock" component={ClockScreen} />
+			<Stack.Screen
+				name="notice"
+				component={NoticeScreen}
+				options={{
+					cardStyleInterpolator: forFade,
+				}}
+			/>
+		</Stack.Navigator>
+	);
+};
 
 export { StackNavigator };
